4ff1fa2e0112a320c6ee8e09877f52bfa23a436a
Kai NeSmith <nesmithkai@gmail.com>
2023-12-14 15:58:02 -0600

    Add project files.

---
+++ IronFE/Time/DosDateTime.cs
@@ -0,0 +1,17 @@
+﻿namespace IronFE.Time
+{
+    public class DosDateTime
+    {
+        private byte _year;
+        private byte _month;
+        private byte _day;
+        private byte _hour;
+        private byte _minute;
+        private byte _second;
+
+        public DosDateTime()
+        {
+
+        }
+    }
+}
