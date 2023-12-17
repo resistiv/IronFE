444f108dcb37544daff9f3f298e469f5c75dddc3
Kai NeSmith <nesmithkai@gmail.com>
2023-12-17 03:31:04 -0600

    Add basic benchmark project

---
+++ IronFE.Benchmark/BitReverserBenchmark.cs
@@ -0,0 +1,41 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Jobs;
+using BenchmarkDotNet.Toolchains.InProcess.NoEmit;
+using IronFE.Tool;
+
+namespace IronFE.Test
+{
+    public class Config : ManualConfig
+    {
+        public Config()
+        {
+            AddJob(Job.MediumRun.WithToolchain(InProcessNoEmitToolchain.Instance));
+        }
+    }
+
+    [Config(typeof(Config))]
+    [MemoryDiagnoser]
+    public class BitReverserBenchmark
+    {
+        private int reverseCount = 5000000;
+
+        [Benchmark]
+        public void ReverseUsingVariableLength()
+        {
+            for (int i = 0; i < reverseCount; i++)
+            {
+                BitReverser.ReverseValue((ulong)i, 64);
+            }
+        }
+
+        [Benchmark]
+        public void ReverseUsingTable()
+        {
+            for (int i = 0; i < reverseCount; i++)
+            {
+                BitReverser.ReverseUInt64((ulong)i);
+            }
+        }
+    }
+}
---
+++ IronFE.Benchmark/Program.cs
@@ -0,0 +1,13 @@
+﻿using BenchmarkDotNet.Running;
+using IronFE.Test;
+
+namespace IronFE.Benchmark
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            BenchmarkRunner.Run<BitReverserBenchmark>();
+        }
+    }
+}
