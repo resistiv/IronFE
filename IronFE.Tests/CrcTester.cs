54c769f4e154edfc2eefdeacab6fb6bab0d0593e
Kai NeSmith <nesmithkai@gmail.com>
2023-12-17 02:14:13 -0600

    Create basic tester

---
+++ IronFE.Tests/CrcTester.cs
@@ -0,0 +1,23 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace IronFE.Tests
+{
+    /// <summary>
+    /// Tests functionality of all pre-defined CRC configurations.
+    /// </summary>
+    [TestClass]
+    public class CrcTester
+    {
+        [TestMethod]
+        public void Crc16Arc()
+        {
+
+        }
+
+        [TestMethod]
+        public void Crc16Xmodem()
+        {
+
+        }
+    }
+}
