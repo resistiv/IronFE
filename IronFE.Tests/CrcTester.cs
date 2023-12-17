ab88c2490c12e1d4d368d9668c88ef9788eb4c75
Kai NeSmith <nesmithkai@gmail.com>
2023-12-17 03:51:15 -0600

    Implement basic CRC tests for ARC & XMODEM

--- IronFE.Tests/CrcTester.cs
+++ IronFE.Tests/CrcTester.cs
@@ -1,4 +1,6 @@
+using IronFE.Hash;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
 
 namespace IronFE.Test
 {
@@ -11,13 +13,29 @@ namespace IronFE.Test
         [TestMethod]
         public void Crc16Arc()
         {
+            Crc arc = new(CrcType.Crc16Arc);
+            string test = "123456789";
 
+            foreach (char c in test)
+            {
+                arc.UpdateCrc(Convert.ToByte(c));
+            }
+
+            Assert.AreEqual((ushort)(arc.Result & 0xFFFF), (ushort)0xBB3D);
         }
 
         [TestMethod]
         public void Crc16Xmodem()
         {
+            Crc xmodem = new(CrcType.Crc16Xmodem);
+            string test = "123456789";
 
+            foreach (char c in test)
+            {
+                xmodem.UpdateCrc(Convert.ToByte(c));
+            }
+
+            Assert.AreEqual((ushort)(xmodem.Result & 0xFFFF), (ushort)0x31C3);
         }
     }
 }
