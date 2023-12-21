d605f2711aa1c6d3f173e730917f094f91ef789d
Kai NeSmith <nesmithkai@gmail.com>
2023-12-21 00:06:28 -0500

    Implement all RevEng CRC-8 presets and tests

---
+++ IronFE.Tests/CrcTests/Crc8Tests.cs
@@ -0,0 +1,235 @@
+﻿using System.Text;
+using IronFE.Hash;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace IronFE.Tests.CrcTests
+{
+    /// <summary>
+    /// Tests functionality of pre-defined CRC-8 configurations.
+    /// </summary>
+    [TestClass]
+    public class Crc8Tests
+    {
+        private static readonly byte[] CheckString = Encoding.ASCII.GetBytes("123456789");
+
+        /// <summary>
+        /// Tests the functionality of the CRC-8/AUTOSAR CRC.
+        /// </summary>
+        [TestMethod]
+        public void Crc8Autosar()
+        {
+            Crc autosar = new(Crc8.Autosar);
+            autosar.Update(CheckString);
+            Assert.AreEqual((ushort)0xDF, (ushort)autosar.Result);
+        }
+
+        /// <summary>
+        /// Tests the functionality of the CRC-8/BLUETOOTH CRC.
+        /// </summary>
+        [TestMethod]
+        public void Crc8Bluetooth()
+        {
+            Crc bluetooth = new(Crc8.Bluetooth);
+            bluetooth.Update(CheckString);
+            Assert.AreEqual((ushort)0x26, (ushort)bluetooth.Result);
+        }
+
+        /// <summary>
+        /// Tests the functionality of the CRC-8/CDMA2000 CRC.
+        /// </summary>
+        [TestMethod]
+        public void Crc8Cdma2000()
+        {
+            Crc cdma2000 = new(Crc8.Cdma2000);
+            cdma2000.Update(CheckString);
+            Assert.AreEqual((ushort)0xDA, (ushort)cdma2000.Result);
+        }
+
+        /// <summary>
+        /// Tests the functionality of the CRC-8/DARC CRC.
+        /// </summary>
+        [TestMethod]
+        public void Crc8Darc()
+        {
+            Crc darc = new(Crc8.Darc);
+            darc.Update(CheckString);
+            Assert.AreEqual((ushort)0x15, (ushort)darc.Result);
+        }
+
+        /// <summary>
+        /// Tests the functionality of the CRC-8/DVB-S2 CRC.
+        /// </summary>
+        [TestMethod]
+        public void Crc8DvbS2()
+        {
+            Crc dvbS2 = new(Crc8.DvbS2);
+            dvbS2.Update(CheckString);
+            Assert.AreEqual((ushort)0xBC, (ushort)dvbS2.Result);
+        }
+
+        /// <summary>
+        /// Tests the functionality of the CRC-8/GSM-A CRC.
+        /// </summary>
+        [TestMethod]
+        public void Crc8GsmA()
+        {
+            Crc gsmA = new(Crc8.GsmA);
+            gsmA.Update(CheckString);
+            Assert.AreEqual((ushort)0x37, (ushort)gsmA.Result);
+        }
+
+        /// <summary>
+        /// Tests the functionality of the CRC-8/GSM-B CRC.
+        /// </summary>
+        [TestMethod]
+        public void Crc8GsmB()
+        {
+            Crc gsmB = new(Crc8.GsmB);
+            gsmB.Update(CheckString);
+            Assert.AreEqual((ushort)0x94, (ushort)gsmB.Result);
+        }
+
+        /// <summary>
+        /// Tests the functionality of the CRC-8/HITAG CRC.
+        /// </summary>
+        [TestMethod]
+        public void Crc8Hitag()
+        {
+            Crc hitag = new(Crc8.Hitag);
+            hitag.Update(CheckString);
+            Assert.AreEqual((ushort)0xB4, (ushort)hitag.Result);
+        }
+
+        /// <summary>
+        /// Tests the functionality of the CRC-8/I-432-1 CRC.
+        /// </summary>
+        [TestMethod]
+        public void Crc8I4321()
+        {
+            Crc i4321 = new(Crc8.I4321);
+            i4321.Update(CheckString);
+            Assert.AreEqual((ushort)0xA1, (ushort)i4321.Result);
+        }
+
+        /// <summary>
+        /// Tests the functionality of the CRC-8/I-CODE CRC.
+        /// </summary>
+        [TestMethod]
+        public void Crc8ICode()
+        {
+            Crc iCode = new(Crc8.ICode);
+            iCode.Update(CheckString);
+            Assert.AreEqual((ushort)0x7E, (ushort)iCode.Result);
+        }
+
+        /// <summary>
+        /// Tests the functionality of the CRC-8/LTE CRC.
+        /// </summary>
+        [TestMethod]
+        public void Crc8Lte()
+        {
+            Crc lte = new(Crc8.Lte);
+            lte.Update(CheckString);
+            Assert.AreEqual((ushort)0xEA, (ushort)lte.Result);
+        }
+
+        /// <summary>
+        /// Tests the functionality of the CRC-8/MAXIM-DOW CRC.
+        /// </summary>
+        [TestMethod]
+        public void Crc8MaximDow()
+        {
+            Crc maximDow = new(Crc8.MaximDow);
+            maximDow.Update(CheckString);
+            Assert.AreEqual((ushort)0xA1, (ushort)maximDow.Result);
+        }
+
+        /// <summary>
+        /// Tests the functionality of the CRC-8/MIFARE-MAD CRC.
+        /// </summary>
+        [TestMethod]
+        public void Crc8MifareMad()
+        {
+            Crc mifareMad = new(Crc8.MifareMad);
+            mifareMad.Update(CheckString);
+            Assert.AreEqual((ushort)0x99, (ushort)mifareMad.Result);
+        }
+
+        /// <summary>
+        /// Tests the functionality of the CRC-8/NRSC-5 CRC.
+        /// </summary>
+        [TestMethod]
+        public void Crc8Nrsc5()
+        {
+            Crc nrsc5 = new(Crc8.Nrsc5);
+            nrsc5.Update(CheckString);
+            Assert.AreEqual((ushort)0xF7, (ushort)nrsc5.Result);
+        }
+
+        /// <summary>
+        /// Tests the functionality of the CRC-8/OPENSAFETY CRC.
+        /// </summary>
+        [TestMethod]
+        public void Crc8OpenSafety()
+        {
+            Crc openSafety = new(Crc8.OpenSafety);
+            openSafety.Update(CheckString);
+            Assert.AreEqual((ushort)0x3E, (ushort)openSafety.Result);
+        }
+
+        /// <summary>
+        /// Tests the functionality of the CRC-8/ROHC CRC.
+        /// </summary>
+        [TestMethod]
+        public void Crc8Rohc()
+        {
+            Crc rohc = new(Crc8.Rohc);
+            rohc.Update(CheckString);
+            Assert.AreEqual((ushort)0xD0, (ushort)rohc.Result);
+        }
+
+        /// <summary>
+        /// Tests the functionality of the CRC-8/SAE-J1850 CRC.
+        /// </summary>
+        [TestMethod]
+        public void Crc8SaeJ1850()
+        {
+            Crc saeJ1850 = new(Crc8.SaeJ1850);
+            saeJ1850.Update(CheckString);
+            Assert.AreEqual((ushort)0x4B, (ushort)saeJ1850.Result);
+        }
+
+        /// <summary>
+        /// Tests the functionality of the CRC-8/SMBUS CRC.
+        /// </summary>
+        [TestMethod]
+        public void Crc8SmBus()
+        {
+            Crc smBus = new(Crc8.SmBus);
+            smBus.Update(CheckString);
+            Assert.AreEqual((ushort)0xF4, (ushort)smBus.Result);
+        }
+
+        /// <summary>
+        /// Tests the functionality of the CRC-8/TECH-3250 CRC.
+        /// </summary>
+        [TestMethod]
+        public void Crc8Tech3250()
+        {
+            Crc tech3250 = new(Crc8.Tech3250);
+            tech3250.Update(CheckString);
+            Assert.AreEqual((ushort)0x97, (ushort)tech3250.Result);
+        }
+
+        /// <summary>
+        /// Tests the functionality of the CRC-8/WCDMA CRC.
+        /// </summary>
+        [TestMethod]
+        public void Crc8Wcdma()
+        {
+            Crc wcdma = new(Crc8.Wcdma);
+            wcdma.Update(CheckString);
+            Assert.AreEqual((ushort)0x25, (ushort)wcdma.Result);
+        }
+    }
+}
---
+++ IronFE/Hash/Crc8.cs
@@ -0,0 +1,288 @@
+﻿namespace IronFE.Hash
+{
+    /// <summary>
+    /// Stores pre-defined CRC-8 parameter configurations.
+    /// </summary>
+    public static class Crc8
+    {
+        /// <summary>
+        /// Gets the CRC-8/AUTOSAR configuration.
+        /// </summary>
+        /// <remarks>
+        /// See <see href="https://reveng.sourceforge.io/crc-catalogue/1-15.htm#crc.cat.crc-8-autosar">CRC-8/AUTOSAR</see> in the RevEng catalogue.
+        /// </remarks>
+        public static CrcParameters Autosar
+        {
+            get
+            {
+                return new("CRC-8/AUTOSAR", 8, 0x2F, 0xFF, false, false, 0xFF);
+            }
+        }
+
+        /// <summary>
+        /// Gets the CRC-8/BLUETOOTH configuration.
+        /// </summary>
+        /// <remarks>
+        /// See <see href="https://reveng.sourceforge.io/crc-catalogue/1-15.htm#crc.cat.crc-8-bluetooth">CRC-8/BLUETOOTH</see> in the RevEng catalogue.
+        /// </remarks>
+        public static CrcParameters Bluetooth
+        {
+            get
+            {
+                return new("CRC-8/BLUETOOTH", 8, 0xA7, 0UL, true, true, 0UL);
+            }
+        }
+
+        /// <summary>
+        /// Gets the CRC-8/CDMA2000 configuration.
+        /// </summary>
+        /// <remarks>
+        /// See <see href="https://reveng.sourceforge.io/crc-catalogue/1-15.htm#crc.cat.crc-8-cdma2000">CRC-8/CDMA2000</see> in the RevEng catalogue.
+        /// </remarks>
+        public static CrcParameters Cdma2000
+        {
+            get
+            {
+                return new("CRC-8/CDMA2000", 8, 0x9B, 0xFF, false, false, 0UL);
+            }
+        }
+
+        /// <summary>
+        /// Gets the CRC-8/DARC configuration.
+        /// </summary>
+        /// <remarks>
+        /// See <see href="https://reveng.sourceforge.io/crc-catalogue/1-15.htm#crc.cat.crc-8-darc">CRC-8/DARC</see> in the RevEng catalogue.
+        /// </remarks>
+        public static CrcParameters Darc
+        {
+            get
+            {
+                return new("CRC-8/DARC", 8, 0x39, 0UL, true, true, 0UL);
+            }
+        }
+
+        /// <summary>
+        /// Gets the CRC-8/DVB-S2 configuration.
+        /// </summary>
+        /// <remarks>
+        /// See <see href="https://reveng.sourceforge.io/crc-catalogue/1-15.htm#crc.cat.crc-8-dvb-s2">CRC-8/DVB-S2</see> in the RevEng catalogue.
+        /// </remarks>
+        public static CrcParameters DvbS2
+        {
+            get
+            {
+                return new("CRC-8/DVB-S2", 8, 0xD5, 0UL, false, false, 0UL);
+            }
+        }
+
+        /// <summary>
+        /// Gets the CRC-8/GSM-A configuration.
+        /// </summary>
+        /// <remarks>
+        /// See <see href="https://reveng.sourceforge.io/crc-catalogue/1-15.htm#crc.cat.crc-8-gsm-a">CRC-8/GSM-A</see> in the RevEng catalogue.
+        /// </remarks>
+        public static CrcParameters GsmA
+        {
+            get
+            {
+                return new("CRC-8/GSM-A", 8, 0x1D, 0UL, false, false, 0UL);
+            }
+        }
+
+        /// <summary>
+        /// Gets the CRC-8/GSM-B configuration.
+        /// </summary>
+        /// <remarks>
+        /// See <see href="https://reveng.sourceforge.io/crc-catalogue/1-15.htm#crc.cat.crc-8-gsm-b">CRC-8/GSM-B</see> in the RevEng catalogue.
+        /// </remarks>
+        public static CrcParameters GsmB
+        {
+            get
+            {
+                return new("CRC-8/GSM-B", 8, 0x49, 0UL, false, false, 0xFF);
+            }
+        }
+
+        /// <summary>
+        /// Gets the CRC-8/HITAG configuration.
+        /// </summary>
+        /// <remarks>
+        /// See <see href="https://reveng.sourceforge.io/crc-catalogue/1-15.htm#crc.cat.crc-8-hitag">CRC-8/HITAG</see> in the RevEng catalogue.
+        /// </remarks>
+        public static CrcParameters Hitag
+        {
+            get
+            {
+                return new("CRC-8/HITAG", 8, 0x1D, 0xFF, false, false, 0UL);
+            }
+        }
+
+        /// <summary>
+        /// Gets the CRC-8/I-432-1 configuration.
+        /// </summary>
+        /// <remarks>
+        /// See <see href="https://reveng.sourceforge.io/crc-catalogue/1-15.htm#crc.cat.crc-8-i-432-1">CRC-8/I-432-1</see> in the RevEng catalogue.
+        /// </remarks>
+        public static CrcParameters I4321
+        {
+            get
+            {
+                return new("CRC-8/I-432-1", 8, 0x07, 0UL, false, false, 0x55);
+            }
+        }
+
+        /// <summary>
+        /// Gets the CRC-8/I-CODE configuration.
+        /// </summary>
+        /// <remarks>
+        /// See <see href="https://reveng.sourceforge.io/crc-catalogue/1-15.htm#crc.cat.crc-8-i-code">CRC-8/I-CODE</see> in the RevEng catalogue.
+        /// </remarks>
+        public static CrcParameters ICode
+        {
+            get
+            {
+                return new("CRC-8/I-CODE", 8, 0x1D, 0xFD, false, false, 0UL);
+            }
+        }
+
+        /// <summary>
+        /// Gets the CRC-8/LTE configuration.
+        /// </summary>
+        /// <remarks>
+        /// See <see href="https://reveng.sourceforge.io/crc-catalogue/1-15.htm#crc.cat.crc-8-lte">CRC-8/LTE</see> in the RevEng catalogue.
+        /// </remarks>
+        public static CrcParameters Lte
+        {
+            get
+            {
+                return new("CRC-8/LTE", 8, 0x9B, 0UL, false, false, 0UL);
+            }
+        }
+
+        /// <summary>
+        /// Gets the CRC-8/MAXIM-DOW configuration.
+        /// </summary>
+        /// <remarks>
+        /// See <see href="https://reveng.sourceforge.io/crc-catalogue/1-15.htm#crc.cat.crc-8-maxim-dow">CRC-8/MAXIM-DOW</see> in the RevEng catalogue.
+        /// </remarks>
+        public static CrcParameters MaximDow
+        {
+            get
+            {
+                return new("CRC-8/MAXIM-DOW", 8, 0x31, 0UL, true, true, 0UL);
+            }
+        }
+
+        /// <summary>
+        /// Gets the CRC-8/MIFARE-MAD configuration.
+        /// </summary>
+        /// <remarks>
+        /// See <see href="https://reveng.sourceforge.io/crc-catalogue/1-15.htm#crc.cat.crc-8-mifare-mad">CRC-8/MIFARE-MAD</see> in the RevEng catalogue.
+        /// </remarks>
+        public static CrcParameters MifareMad
+        {
+            get
+            {
+                return new("CRC-8/MIFARE-MAD", 8, 0x1D, 0xC7, false, false, 0UL);
+            }
+        }
+
+        /// <summary>
+        /// Gets the CRC-8/NRSC-5 configuration.
+        /// </summary>
+        /// <remarks>
+        /// See <see href="https://reveng.sourceforge.io/crc-catalogue/1-15.htm#crc.cat.crc-8-nrsc-5">CRC-8/NRSC-5</see> in the RevEng catalogue.
+        /// </remarks>
+        public static CrcParameters Nrsc5
+        {
+            get
+            {
+                return new("CRC-8/NRSC-5", 8, 0x31, 0xFF, false, false, 0UL);
+            }
+        }
+
+        /// <summary>
+        /// Gets the CRC-8/OPENSAFETY configuration.
+        /// </summary>
+        /// <remarks>
+        /// See <see href="https://reveng.sourceforge.io/crc-catalogue/1-15.htm#crc.cat.crc-8-opensafety">CRC-8/OPENSAFETY</see> in the RevEng catalogue.
+        /// </remarks>
+        public static CrcParameters OpenSafety
+        {
+            get
+            {
+                return new("CRC-8/OPENSAFETY", 8, 0x2F, 0UL, false, false, 0UL);
+            }
+        }
+
+        /// <summary>
+        /// Gets the CRC-8/ROHC configuration.
+        /// </summary>
+        /// <remarks>
+        /// See <see href="https://reveng.sourceforge.io/crc-catalogue/1-15.htm#crc.cat.crc-8-rohc">CRC-8/ROHC</see> in the RevEng catalogue.
+        /// </remarks>
+        public static CrcParameters Rohc
+        {
+            get
+            {
+                return new("CRC-8/OPENSAFETY", 8, 0x07, 0xFF, true, true, 0UL);
+            }
+        }
+
+        /// <summary>
+        /// Gets the CRC-8/SAE-J1850 configuration.
+        /// </summary>
+        /// <remarks>
+        /// See <see href="https://reveng.sourceforge.io/crc-catalogue/1-15.htm#crc.cat.crc-8-sae-j1850">CRC-8/SAE-J1850</see> in the RevEng catalogue.
+        /// </remarks>
+        public static CrcParameters SaeJ1850
+        {
+            get
+            {
+                return new("CRC-8/SAE-J1850", 8, 0x1D, 0xFF, false, false, 0xFF);
+            }
+        }
+
+        /// <summary>
+        /// Gets the CRC-8/SMBUS configuration.
+        /// </summary>
+        /// <remarks>
+        /// See <see href="https://reveng.sourceforge.io/crc-catalogue/1-15.htm#crc.cat.crc-8-smbus">CRC-8/SMBUS</see> in the RevEng catalogue.
+        /// </remarks>
+        public static CrcParameters SmBus
+        {
+            get
+            {
+                return new("CRC-8/SMBUS", 8, 0x07, 0UL, false, false, 0UL);
+            }
+        }
+
+        /// <summary>
+        /// Gets the CRC-8/TECH-3250 configuration.
+        /// </summary>
+        /// <remarks>
+        /// See <see href="https://reveng.sourceforge.io/crc-catalogue/1-15.htm#crc.cat.crc-8-tech-3250">CRC-8/TECH-3250</see> in the RevEng catalogue.
+        /// </remarks>
+        public static CrcParameters Tech3250
+        {
+            get
+            {
+                return new("CRC-8/TECH-3250", 8, 0x1D, 0xFF, true, true, 0UL);
+            }
+        }
+
+        /// <summary>
+        /// Gets the CRC-8/WCDMA configuration.
+        /// </summary>
+        /// <remarks>
+        /// See <see href="https://reveng.sourceforge.io/crc-catalogue/1-15.htm#crc.cat.crc-8-wcdma">CRC-8/WCDMA</see> in the RevEng catalogue.
+        /// </remarks>
+        public static CrcParameters Wcdma
+        {
+            get
+            {
+                return new("CRC-8/WCDMA", 8, 0x9B, 0UL, true, true, 0UL);
+            }
+        }
+    }
+}
