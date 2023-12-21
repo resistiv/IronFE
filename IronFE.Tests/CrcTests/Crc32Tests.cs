4254647545eeffca985cc89e42a198710edf6486
Kai NeSmith <nesmithkai@gmail.com>
2023-12-21 01:00:02 -0500

    Implement all RevEng CRC-32 presets and tests

---
+++ IronFE.Tests/CrcTests/Crc32Tests.cs
@@ -0,0 +1,147 @@
+﻿using System.Text;
+using IronFE.Hash;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace IronFE.Tests.CrcTests
+{
+    /// <summary>
+    /// Tests functionality of pre-defined CRC-32 configurations.
+    /// </summary>
+    [TestClass]
+    public class Crc32Tests
+    {
+        private static readonly byte[] CheckString = Encoding.ASCII.GetBytes("123456789");
+
+        /// <summary>
+        /// Tests the functionality of the CRC-32/AIXM CRC.
+        /// </summary>
+        [TestMethod]
+        public void Crc32Aixm()
+        {
+            Crc aixm = new(Crc32.Aixm);
+            aixm.Update(CheckString);
+            Assert.AreEqual(0x3010BF7FU, (uint)aixm.Result);
+        }
+
+        /// <summary>
+        /// Tests the functionality of the CRC-32/AUTOSAR CRC.
+        /// </summary>
+        [TestMethod]
+        public void Crc32Autosar()
+        {
+            Crc autosar = new(Crc32.Autosar);
+            autosar.Update(CheckString);
+            Assert.AreEqual(0x1697D06AU, (uint)autosar.Result);
+        }
+
+        /// <summary>
+        /// Tests the functionality of the CRC-32/BASE91-D CRC.
+        /// </summary>
+        [TestMethod]
+        public void Crc32Base91D()
+        {
+            Crc base91D = new(Crc32.Base91D);
+            base91D.Update(CheckString);
+            Assert.AreEqual(0x87315576U, (uint)base91D.Result);
+        }
+
+        /// <summary>
+        /// Tests the functionality of the CRC-32/BZIP2 CRC.
+        /// </summary>
+        [TestMethod]
+        public void Crc32Bzip2()
+        {
+            Crc bzip2 = new(Crc32.Bzip2);
+            bzip2.Update(CheckString);
+            Assert.AreEqual(0xFC891918U, (uint)bzip2.Result);
+        }
+
+        /// <summary>
+        /// Tests the functionality of the CRC-32/CD-ROM-EDC CRC.
+        /// </summary>
+        [TestMethod]
+        public void Crc32CdRomEdc()
+        {
+            Crc cdRomEdc = new(Crc32.CdRomEdc);
+            cdRomEdc.Update(CheckString);
+            Assert.AreEqual(0x6EC2EDC4U, (uint)cdRomEdc.Result);
+        }
+
+        /// <summary>
+        /// Tests the functionality of the CRC-32/CKSUM CRC.
+        /// </summary>
+        [TestMethod]
+        public void Crc32Cksum()
+        {
+            Crc cksum = new(Crc32.Cksum);
+            cksum.Update(CheckString);
+            Assert.AreEqual(0x765E7680U, (uint)cksum.Result);
+        }
+
+        /// <summary>
+        /// Tests the functionality of the CRC-32/ISCSI CRC.
+        /// </summary>
+        [TestMethod]
+        public void Crc32Iscsi()
+        {
+            Crc iscsi = new(Crc32.Iscsi);
+            iscsi.Update(CheckString);
+            Assert.AreEqual(0xE3069283U, (uint)iscsi.Result);
+        }
+
+        /// <summary>
+        /// Tests the functionality of the CRC-32/ISO-HDLC CRC.
+        /// </summary>
+        [TestMethod]
+        public void Crc32IsoHdlc()
+        {
+            Crc isoHdlc = new(Crc32.IsoHdlc);
+            isoHdlc.Update(CheckString);
+            Assert.AreEqual(0xCBF43926U, (uint)isoHdlc.Result);
+        }
+
+        /// <summary>
+        /// Tests the functionality of the CRC-32/JAMCRC CRC.
+        /// </summary>
+        [TestMethod]
+        public void Crc32JamCrc()
+        {
+            Crc jamCrc = new(Crc32.JamCrc);
+            jamCrc.Update(CheckString);
+            Assert.AreEqual(0x340BC6D9U, (uint)jamCrc.Result);
+        }
+
+        /// <summary>
+        /// Tests the functionality of the CRC-32/MEF CRC.
+        /// </summary>
+        [TestMethod]
+        public void Crc32Mef()
+        {
+            Crc mef = new(Crc32.Mef);
+            mef.Update(CheckString);
+            Assert.AreEqual(0xD2C22F51U, (uint)mef.Result);
+        }
+
+        /// <summary>
+        /// Tests the functionality of the CRC-32/MPEG-2 CRC.
+        /// </summary>
+        [TestMethod]
+        public void Crc32Mpeg2()
+        {
+            Crc mpeg2 = new(Crc32.Mpeg2);
+            mpeg2.Update(CheckString);
+            Assert.AreEqual(0x0376E6E7U, (uint)mpeg2.Result);
+        }
+
+        /// <summary>
+        /// Tests the functionality of the CRC-32/XFER CRC.
+        /// </summary>
+        [TestMethod]
+        public void Crc32Xfer()
+        {
+            Crc xfer = new(Crc32.Xfer);
+            xfer.Update(CheckString);
+            Assert.AreEqual(0xBD0BE338U, (uint)xfer.Result);
+        }
+    }
+}
---
+++ IronFE/Hash/Crc32.cs
@@ -0,0 +1,176 @@
+﻿namespace IronFE.Hash
+{
+    /// <summary>
+    /// Stores pre-defined CRC-32 parameter configurations.
+    /// </summary>
+    public static class Crc32
+    {
+        /// <summary>
+        /// Gets the CRC-32/AIXM configuration.
+        /// </summary>
+        /// <remarks>
+        /// See <see href="https://reveng.sourceforge.io/crc-catalogue/17plus.htm#crc.cat.crc-32-aixm">CRC-32/AIXM</see> in the RevEng catalogue.
+        /// </remarks>
+        public static CrcParameters Aixm
+        {
+            get
+            {
+                return new("CRC-32/AIXM", 32, 0x814141AB, 0UL, false, false, 0UL);
+            }
+        }
+
+        /// <summary>
+        /// Gets the CRC-32/AUTOSAR configuration.
+        /// </summary>
+        /// <remarks>
+        /// See <see href="https://reveng.sourceforge.io/crc-catalogue/17plus.htm#crc.cat.crc-32-autosar">CRC-32/AUTOSAR</see> in the RevEng catalogue.
+        /// </remarks>
+        public static CrcParameters Autosar
+        {
+            get
+            {
+                return new("CRC-32/AUTOSAR", 32, 0xF4ACFB13, 0xFFFFFFFF, true, true, 0xFFFFFFFF);
+            }
+        }
+
+        /// <summary>
+        /// Gets the CRC-32/BASE91-D configuration.
+        /// </summary>
+        /// <remarks>
+        /// See <see href="https://reveng.sourceforge.io/crc-catalogue/17plus.htm#crc.cat.crc-32-base91-d">CRC-32/BASE91-D</see> in the RevEng catalogue.
+        /// </remarks>
+        public static CrcParameters Base91D
+        {
+            get
+            {
+                return new("CRC-32/BASE91-D", 32, 0xA833982B, 0xFFFFFFFF, true, true, 0xFFFFFFFF);
+            }
+        }
+
+        /// <summary>
+        /// Gets the CRC-32/BZIP2 configuration.
+        /// </summary>
+        /// <remarks>
+        /// See <see href="https://reveng.sourceforge.io/crc-catalogue/17plus.htm#crc.cat.crc-32-bzip2">CRC-32/BZIP2</see> in the RevEng catalogue.
+        /// </remarks>
+        public static CrcParameters Bzip2
+        {
+            get
+            {
+                return new("CRC-32/BZIP2", 32, 0x04C11DB7, 0xFFFFFFFF, false, false, 0xFFFFFFFF);
+            }
+        }
+
+        /// <summary>
+        /// Gets the CRC-32/CD-ROM-EDC configuration.
+        /// </summary>
+        /// <remarks>
+        /// See <see href="https://reveng.sourceforge.io/crc-catalogue/17plus.htm#crc.cat.crc-32-cd-rom-edc">CRC-32/CD-ROM-EDC</see> in the RevEng catalogue.
+        /// </remarks>
+        public static CrcParameters CdRomEdc
+        {
+            get
+            {
+                return new("CRC-32/CD-ROM-EDC", 32, 0x8001801B, 0UL, true, true, 0UL);
+            }
+        }
+
+        /// <summary>
+        /// Gets the CRC-32/CKSUM configuration.
+        /// </summary>
+        /// <remarks>
+        /// See <see href="https://reveng.sourceforge.io/crc-catalogue/17plus.htm#crc.cat.crc-32-cksum">CRC-32/CKSUM</see> in the RevEng catalogue.
+        /// </remarks>
+        public static CrcParameters Cksum
+        {
+            get
+            {
+                return new("CRC-32/CKSUM", 32, 0x04C11DB7, 0UL, false, false, 0xFFFFFFFF);
+            }
+        }
+
+        /// <summary>
+        /// Gets the CRC-32/ISCSI configuration.
+        /// </summary>
+        /// <remarks>
+        /// See <see href="https://reveng.sourceforge.io/crc-catalogue/17plus.htm#crc.cat.crc-32-iscsi">CRC-32/ISCSI</see> in the RevEng catalogue.
+        /// </remarks>
+        public static CrcParameters Iscsi
+        {
+            get
+            {
+                return new("CRC-32/ISCSI", 32, 0x1EDC6F41, 0xFFFFFFFF, true, true, 0xFFFFFFFF);
+            }
+        }
+
+        /// <summary>
+        /// Gets the CRC-32/ISO-HDLC configuration.
+        /// </summary>
+        /// <remarks>
+        /// See <see href="https://reveng.sourceforge.io/crc-catalogue/17plus.htm#crc.cat.crc-32-iso-hdlc">CRC-32/ISO-HDLC</see> in the RevEng catalogue.
+        /// </remarks>
+        public static CrcParameters IsoHdlc
+        {
+            get
+            {
+                return new("CRC-32/ISO-HDLC", 32, 0x04C11DB7, 0xFFFFFFFF, true, true, 0xFFFFFFFF);
+            }
+        }
+
+        /// <summary>
+        /// Gets the CRC-32/JAMCRC configuration.
+        /// </summary>
+        /// <remarks>
+        /// See <see href="https://reveng.sourceforge.io/crc-catalogue/17plus.htm#crc.cat.crc-32-jamcrc">CRC-32/JAMCRC</see> in the RevEng catalogue.
+        /// </remarks>
+        public static CrcParameters JamCrc
+        {
+            get
+            {
+                return new("CRC-32/JAMCRC", 32, 0x04C11DB7, 0xFFFFFFFF, true, true, 0UL);
+            }
+        }
+
+        /// <summary>
+        /// Gets the CRC-32/MEF configuration.
+        /// </summary>
+        /// <remarks>
+        /// See <see href="https://reveng.sourceforge.io/crc-catalogue/17plus.htm#crc.cat.crc-32-mef">CRC-32/MEF</see> in the RevEng catalogue.
+        /// </remarks>
+        public static CrcParameters Mef
+        {
+            get
+            {
+                return new("CRC-32/MEF", 32, 0x741B8CD7, 0xFFFFFFFF, true, true, 0UL);
+            }
+        }
+
+        /// <summary>
+        /// Gets the CRC-32/MPEG-2 configuration.
+        /// </summary>
+        /// <remarks>
+        /// See <see href="https://reveng.sourceforge.io/crc-catalogue/17plus.htm#crc.cat.crc-32-mpeg-2">CRC-32/MPEG-2</see> in the RevEng catalogue.
+        /// </remarks>
+        public static CrcParameters Mpeg2
+        {
+            get
+            {
+                return new("CRC-32/MPEG-2", 32, 0x04C11DB7, 0xFFFFFFFF, false, false, 0UL);
+            }
+        }
+
+        /// <summary>
+        /// Gets the CRC-32/XFER configuration.
+        /// </summary>
+        /// <remarks>
+        /// See <see href="https://reveng.sourceforge.io/crc-catalogue/17plus.htm#crc.cat.crc-32-xfer">CRC-32/XFER</see> in the RevEng catalogue.
+        /// </remarks>
+        public static CrcParameters Xfer
+        {
+            get
+            {
+                return new("CRC-32/XFER", 32, 0x000000AF, 0UL, false, false, 0UL);
+            }
+        }
+    }
+}
