637112720ff20a52019908810b8f1bd10595f0f9
Kai NeSmith <nesmithkai@gmail.com>
2023-12-21 21:56:29 -0500

    Implement all RevEng CRC-64 presets and tests

---
+++ IronFE.Tests/HashTests/Crc64Tests.cs
@@ -0,0 +1,81 @@
+﻿using System.Text;
+using IronFE.Hash;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace IronFE.Tests.HashTests
+{
+    /// <summary>
+    /// Tests functionality of pre-defined CRC-64 configurations.
+    /// </summary>
+    [TestClass]
+    public class Crc64Tests
+    {
+        private static readonly byte[] CheckString = Encoding.ASCII.GetBytes("123456789");
+
+        /// <summary>
+        /// Tests the functionality of the CRC-64/ECMA-182 CRC.
+        /// </summary>
+        [TestMethod]
+        public void Crc64Ecma182()
+        {
+            Crc ecma182 = new(Crc64.Ecma182);
+            ecma182.Update(CheckString);
+            Assert.AreEqual(0x6C40DF5F0B497347UL, ecma182.Result);
+        }
+
+        /// <summary>
+        /// Tests the functionality of the CRC-64/GO-ISO CRC.
+        /// </summary>
+        [TestMethod]
+        public void Crc64GoIso()
+        {
+            Crc goIso = new(Crc64.GoIso);
+            goIso.Update(CheckString);
+            Assert.AreEqual(0xB90956C775A41001UL, goIso.Result);
+        }
+
+        /// <summary>
+        /// Tests the functionality of the CRC-64/MS CRC.
+        /// </summary>
+        [TestMethod]
+        public void Crc64Ms()
+        {
+            Crc ms = new(Crc64.Ms);
+            ms.Update(CheckString);
+            Assert.AreEqual(0x75D4B74F024ECEEAUL, ms.Result);
+        }
+
+        /// <summary>
+        /// Tests the functionality of the CRC-64/REDIS CRC.
+        /// </summary>
+        [TestMethod]
+        public void Crc64Redis()
+        {
+            Crc redis = new(Crc64.Redis);
+            redis.Update(CheckString);
+            Assert.AreEqual(0xE9C6D914C4B8d9CAUL, redis.Result);
+        }
+
+        /// <summary>
+        /// Tests the functionality of the CRC-64/WE CRC.
+        /// </summary>
+        [TestMethod]
+        public void Crc64We()
+        {
+            Crc we = new(Crc64.We);
+            we.Update(CheckString);
+            Assert.AreEqual(0x62EC59E3F1A4F00AUL, we.Result);
+        }
+
+        /// <summary>
+        /// Tests the functionality of the CRC-64/XZ CRC.
+        /// </summary>
+        [TestMethod]
+        public void Crc64Xz()
+        {
+            Crc xz = new(Crc64.Xz);
+            xz.Update(CheckString);
+            Assert.AreEqual(0x995DC9BBDF1939FAUL, xz.Result);
+        }
+    }
+}
---
+++ IronFE/Hash/Crc64.cs
@@ -0,0 +1,92 @@
+﻿namespace IronFE.Hash
+{
+    /// <summary>
+    /// Stores pre-defined CRC-64 parameter configurations.
+    /// </summary>
+    public static class Crc64
+    {
+        /// <summary>
+        /// Gets the CRC-64/ECMA-182 configuration.
+        /// </summary>
+        /// <remarks>
+        /// See <see href="https://reveng.sourceforge.io/crc-catalogue/17plus.htm#crc.cat.crc-64-ecma-182">CRC-64/ECMA-182</see> in the RevEng catalogue.
+        /// </remarks>
+        public static CrcParameters Ecma182
+        {
+            get
+            {
+                return new("CRC-64/ECMA-182", 64, 0x42F0E1EBA9EA3693, 0UL, false, false, 0UL);
+            }
+        }
+
+        /// <summary>
+        /// Gets the CRC-64/GO-ISO configuration.
+        /// </summary>
+        /// <remarks>
+        /// See <see href="https://reveng.sourceforge.io/crc-catalogue/17plus.htm#crc.cat.crc-64-go-iso">CRC-64/GO-ISO</see> in the RevEng catalogue.
+        /// </remarks>
+        public static CrcParameters GoIso
+        {
+            get
+            {
+                return new("CRC-64/GO-ISO", 64, 0x000000000000001B, 0xFFFFFFFFFFFFFFFF, true, true, 0xFFFFFFFFFFFFFFFF);
+            }
+        }
+
+        /// <summary>
+        /// Gets the CRC-64/MS configuration.
+        /// </summary>
+        /// <remarks>
+        /// See <see href="https://reveng.sourceforge.io/crc-catalogue/17plus.htm#crc.cat.crc-64-ms">CRC-64/MS</see> in the RevEng catalogue.
+        /// </remarks>
+        public static CrcParameters Ms
+        {
+            get
+            {
+                return new("CRC-64/MS", 64, 0x259C84CBA6426349, 0xFFFFFFFFFFFFFFFF, true, true, 0UL);
+            }
+        }
+
+        /// <summary>
+        /// Gets the CRC-64/REDIS configuration.
+        /// </summary>
+        /// <remarks>
+        /// See <see href="https://reveng.sourceforge.io/crc-catalogue/17plus.htm#crc.cat.crc-64-redis">CRC-64/REDIS</see> in the RevEng catalogue.
+        /// </remarks>
+        public static CrcParameters Redis
+        {
+            get
+            {
+                return new("CRC-64/REDIS", 64, 0xAD93D23594C935A9, 0UL, true, true, 0UL);
+            }
+        }
+
+        /// <summary>
+        /// Gets the CRC-64/WE configuration.
+        /// </summary>
+        /// <remarks>
+        /// See <see href="https://reveng.sourceforge.io/crc-catalogue/17plus.htm#crc.cat.crc-64-we">CRC-64/WE</see> in the RevEng catalogue.
+        /// </remarks>
+        public static CrcParameters We
+        {
+            get
+            {
+                return new("CRC-64/WE", 64, 0x42F0E1EBA9EA3693, 0xFFFFFFFFFFFFFFFF, false, false, 0xFFFFFFFFFFFFFFFF);
+            }
+        }
+
+        /// <summary>
+        /// Gets the CRC-64/XZ configuration.
+        /// </summary>
+        /// <remarks>
+        /// See <see href="https://reveng.sourceforge.io/crc-catalogue/17plus.htm#crc.cat.crc-64-xz">CRC-64/XZ</see> in the RevEng catalogue.
+        /// </remarks>
+        public static CrcParameters Xz
+        {
+            get
+            {
+                return new("CRC-64/XZ", 64, 0x42F0E1EBA9EA3693, 0xFFFFFFFFFFFFFFFF, true, true, 0xFFFFFFFFFFFFFFFF);
+            }
+        }
+    }
+}
