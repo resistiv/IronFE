de8535403321cbd90a838f30871a0965b17cd004
Kai NeSmith <nesmithkai@gmail.com>
2023-12-27 22:31:44 -0500

    Restructure Decoder to inherit from Coder

--- IronFE/Encoding/Decoder.cs
+++ IronFE/Encoding/Decoder.cs
@@ -1,22 +1,18 @@
-﻿using System;
-using System.IO;
+﻿using System.IO;
 
 namespace IronFE.Encoding
 {
     /// <summary>
     /// Provides a mechanism for decoding an encoded stream of data.
     /// </summary>
-    public abstract class Decoder : IDisposable
+    public abstract class Decoder : Coder
     {
-        private readonly bool leaveOpen;
-        private bool disposed = false;
-
         /// <summary>
         /// Initializes a new instance of the <see cref="Decoder"/> class over a specified <see cref="Stream"/> which will assume the rest of the stream is encoded and will not close the stream when disposed of.
         /// </summary>
         /// <param name="stream">A <see cref="Stream"/> containing data to be decoded.</param>
         public Decoder(Stream stream)
-            : this(stream, stream.Length - stream.Position)
+            : base(stream)
         {
         }
 
@@ -26,7 +22,7 @@ namespace IronFE.Encoding
         /// <param name="stream">A <see cref="Stream"/> containing data to be decoded.</param>
         /// <param name="streamLength">The length of the encoded data section in <paramref name="stream"/>.</param>
         public Decoder(Stream stream, long streamLength)
-            : this(stream, streamLength, true)
+            : base(stream, streamLength)
         {
         }
 
@@ -37,50 +33,14 @@ namespace IronFE.Encoding
         /// <param name="streamLength">The length of the encoded data section in <paramref name="stream"/>.</param>
         /// <param name="leaveOpen">Whether or not to leave <paramref name="stream"/> open when this instance is disposed.</param>
         public Decoder(Stream stream, long streamLength, bool leaveOpen)
+            : base(stream, streamLength, leaveOpen)
         {
-            BaseStream = stream ?? throw new ArgumentNullException(nameof(stream), "Cannot pass a null Stream to a Decoder.");
-            StreamLength = streamLength;
-            this.leaveOpen = leaveOpen;
         }
-
-        /// <summary>
-        /// Gets the underlying <see cref="Stream"/> of this <see cref="Decoder"/>.
-        /// </summary>
-        public Stream BaseStream { get; }
-
-        /// <summary>
-        /// Gets the length of the encoded data section of the underlying <see cref="Stream"/>.
-        /// </summary>
-        public long StreamLength { get; }
 
         /// <summary>
         /// Decodes the underlying <see cref="Stream"/> of this <see cref="Decoder"/> and writes the output to <paramref name="outStream"/>.
         /// </summary>
         /// <param name="outStream">A <see cref="Stream"/> to write decoded output to.</param>
         public abstract void Decode(ref Stream outStream);
-
-        /// <inheritdoc/>
-        public void Dispose()
-        {
-            Dispose(disposing: true);
-            GC.SuppressFinalize(this);
-        }
-
-        /// <summary>
-        /// Disposes resources.
-        /// </summary>
-        /// <param name="disposing">Whether or not managed resources are to be disposed of.</param>
-        protected virtual void Dispose(bool disposing)
-        {
-            if (!disposed)
-            {
-                if (disposing && !leaveOpen)
-                {
-                    BaseStream.Dispose();
-                }
-
-                disposed = true;
-            }
-        }
     }
 }
