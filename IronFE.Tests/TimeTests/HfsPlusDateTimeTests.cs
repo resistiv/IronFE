8372871178148fa0486f81d99bdb48d9bd60a44a
Kai NeSmith <nesmithkai@gmail.com>
2023-12-31 21:47:31 -0500

    Implement HFS+ timestamps & tests

---
+++ IronFE.Tests/TimeTests/HfsPlusDateTimeTests.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using IronFE.Time;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace IronFE.Tests.TimeTests
+{
+    /// <summary>
+    /// Tests functionality of the <see cref="Time.HfsPlusDateTime"/> class.
+    /// </summary>
+    [TestClass]
+    public class HfsPlusDateTimeTests
+    {
+        // Timestamp taken from the MacBinary file on this page: https://www.macintoshrepository.org/479-binhex-4-0
+        private const uint HfsPlusDateTime = 0x9898231B;
+
+        // Converted using https://www.epochconverter.com/mac
+        private static readonly DateTime ConvertedDateTime = new(1985, 2, 14, 20, 54, 51);
+
+        /// <summary>
+        /// Tests functionality of the <see cref="HfsPlusDateTime.ToHfsPlusDateTime(DateTime)"/> extension method.
+        /// </summary>
+        [TestMethod]
+        public void ToHfsPlusDateTime()
+        {
+            uint dateTime = ConvertedDateTime.ToHfsPlusDateTime();
+            Assert.AreEqual(HfsPlusDateTime, dateTime);
+        }
+
+        /// <summary>
+        /// Tests functionality of the <see cref="HfsPlusDateTime.ToHfsPlusDateTime(DateTime)"/> extension method when given a date and time below the minimum HFS+ date and time.
+        /// </summary>
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void ToHfsPlusDateTimeBelowMin()
+        {
+            new DateTime(1903, 12, 31, 23, 59, 59).ToHfsPlusDateTime();
+        }
+
+        /// <summary>
+        /// Tests functionality of the <see cref="HfsPlusDateTime.ToHfsPlusDateTime(DateTime)"/> extension method when given a date and time above the maximum HFS+ date and time.
+        /// </summary>
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void ToHfsPlusDateTimeAboveMax()
+        {
+            new DateTime(2040, 2, 6, 6, 28, 16).ToHfsPlusDateTime();
+        }
+
+        /// <summary>
+        /// Tests functionality of the <see cref="HfsPlusDateTime.ToDateTime(uint)"/> method.
+        /// </summary>
+        [TestMethod]
+        public void ToDateTime()
+        {
+            DateTime dateTime = Time.HfsPlusDateTime.ToDateTime(HfsPlusDateTime);
+            Assert.AreEqual(ConvertedDateTime, dateTime);
+        }
+    }
+}
---
+++ IronFE/Time/HfsPlusDateTime.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace IronFE.Time
+{
+    /// <summary>
+    /// Handles HFS+ date/time conversions.
+    /// </summary>
+    public static class HfsPlusDateTime
+    {
+        private static readonly DateTime MinDateTime = new(1904, 1, 1, 0, 0, 0);
+        private static readonly DateTime MaxDateTime = new(2040, 2, 6, 6, 28, 15);
+
+        /// <summary>
+        /// Converts the current <see cref="DateTime"/> object to an HFS+ date and time.
+        /// </summary>
+        /// <param name="dateTime">A <see cref="DateTime"/>.</param>
+        /// <returns>A 32-bit unsigned integer representing the converted <see cref="DateTime"/> as the number of seconds since 1904/01/01 00:00:00.</returns>
+        public static uint ToHfsPlusDateTime(this DateTime dateTime)
+        {
+            if (dateTime < MinDateTime)
+            {
+                throw new ArgumentOutOfRangeException(nameof(dateTime), "Cannot convert a date & time earlier that 1904/01/01 00:00:00 to HFS+ format.");
+            }
+            else if (dateTime > MaxDateTime)
+            {
+                throw new ArgumentOutOfRangeException(nameof(dateTime), "Cannot convert a date & time later that 2040/02/06 06:28:15 to HFS+ format.");
+            }
+
+            return (uint)dateTime.Subtract(MinDateTime).TotalSeconds;
+        }
+
+        /// <summary>
+        /// Converts an HFS+ date and time into a <see cref="DateTime"/>.
+        /// </summary>
+        /// <param name="hfsPlusDateTime">A 32-bit unsigned integer representing the number of seconds since 1904/01/01 00:00:00.</param>
+        /// <returns>A <see cref="DateTime"/> representing the HFS+ date and time.</returns>
+        public static DateTime ToDateTime(uint hfsPlusDateTime)
+            => MinDateTime.Add(TimeSpan.FromSeconds(hfsPlusDateTime));
+    }
+}
