f84d78794716d9cd55e4d6c90d42732f7a72515f
klnesmith <klnesmith@crimson.ua.edu>
2024-03-04 16:45:00 -0600

    Break out internal reading for DecodingStream, begin documenting

--- IronFE/Encoding/DecodingStream.cs
+++ IronFE/Encoding/DecodingStream.cs
@@ -3,11 +3,19 @@ using System.IO;
 
 namespace IronFE.Encoding
 {
+    /// <summary>
+    /// Provides a mechanism to decode data from an underlying <see cref="Stream"/>.
+    /// </summary>
     public abstract class DecodingStream : Stream
     {
+        /// <summary>
+        /// The default size of the internal buffer used for residual decoding bytes.
+        /// </summary>
         protected const int DefaultBufferSize = 8192;
+        protected byte[] buffer;
+        protected Stream stream;
 
-        protected readonly bool leaveOpen;
+        private readonly bool leaveOpen;
 
         protected DecodingStream(Stream stream)
             : this(stream, false)
@@ -23,42 +31,78 @@ namespace IronFE.Encoding
                 throw new ArgumentException(Properties.Strings.DecodingStreamNotSupportedUnreadableStream, nameof(stream));
             }
 
-            BaseStream = stream;
+            this.stream = stream;
             this.leaveOpen = leaveOpen;
+            this.buffer = new byte[DefaultBufferSize];
         }
 
-        public Stream BaseStream { get; }
+        /// <summary>
+        /// Gets the underlying <see cref="Stream"/> object of this <see cref="DecodingStream"/>.
+        /// </summary>
+        public Stream BaseStream => stream;
 
+
+        /// <inheritdoc/>
         public override bool CanRead => true;
 
+
+        /// <inheritdoc/>
         public override bool CanSeek => false;
 
+
+        /// <inheritdoc/>
         public override bool CanWrite => false;
 
+
+        /// <inheritdoc/>
         public override long Length => throw new NotSupportedException(Properties.Strings.DecodingStreamNotSupported);
 
+
+        /// <inheritdoc/>
         public override long Position
         {
             get => throw new NotSupportedException(Properties.Strings.DecodingStreamNotSupported);
             set => throw new NotSupportedException(Properties.Strings.DecodingStreamNotSupported);
         }
 
+        /// <inheritdoc/>
         public override void Flush()
         {
-            throw new System.NotImplementedException();
+            BaseStream.Flush();
+
+            throw new NotImplementedException();
         }
 
+
+        /// <inheritdoc/>
         public override int Read(byte[] buffer, int offset, int count)
         {
-            throw new System.NotImplementedException();
+            ObjectDisposedException.ThrowIf(stream is null, this);
+            ValidateBufferArguments(buffer, offset, count);
+            return ReadInternal(buffer, offset, count);
         }
 
+        /// <summary>
+        /// Reads a sequence of bytes through an internal decoding operation on the underlying <see cref="Stream"/> object.
+        /// </summary>
+        /// <param name="buffer">An array of <see cref="byte"/>s to output data to.</param>
+        /// <param name="offset">The zero-based byte offset in <paramref name="buffer"/> at which to begin storing the data read from the current stream.</param>
+        /// <param name="count">The maximum number of bytes to be read from the underlying <see cref="Stream"/>.</param>
+        /// <returns>The total number of bytes read into <paramref name="buffer"/>.</returns>
+        protected abstract int ReadInternal(byte[] buffer, int offset, int count);
+
+
+        /// <inheritdoc/>
         public override long Seek(long offset, SeekOrigin origin)
             => throw new NotSupportedException(Properties.Strings.DecodingStreamNotSupported);
 
+
+        /// <inheritdoc/>
         public override void SetLength(long value)
             => throw new NotSupportedException(Properties.Strings.DecodingStreamNotSupported);
 
+
+        /// <inheritdoc/>
         public override void Write(byte[] buffer, int offset, int count)
             => throw new NotSupportedException(Properties.Strings.DecodingStreamNotSupported);
     }
