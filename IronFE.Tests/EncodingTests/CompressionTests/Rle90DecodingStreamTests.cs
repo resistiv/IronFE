a91771ab14d3f950e34365608720d47a86d3a9c9
Kai NeSmith <nesmithkai@gmail.com>
2024-03-31 14:42:43 -0500

    Implement the first Rle90DecodingStream test.
    Reads an ARC RLE90 binary in full with only one Read() call

---
+++ IronFE.Tests/EncodingTests/CompressionTests/Rle90DecodingStreamTests.cs
@@ -0,0 +1,38 @@
+﻿using System.IO;
+using IronFE.Encoding.Compression;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace IronFE.Tests.EncodingTests.CompressionTests
+{
+    /// <summary>
+    /// Tests functionality of the <see cref="Rle90DecodingStream"/> class.
+    /// </summary>
+    [TestClass]
+    [DeploymentItem("TestData/Encoding/Compression/Rle90ArcEncoded.bin", "TestData")]
+    [DeploymentItem("TestData/Encoding/Compression/Rle90BinHexEncoded.bin", "TestData")]
+    public class Rle90DecodingStreamTests : EncodingTests
+    {
+        /// <summary>
+        /// Tests the functionality of the <see cref="Rle90DecodingStream.ReadInternal(byte[], int, int)"/> method given ARC-compressed data read in full.
+        /// </summary>
+        [TestMethod]
+        public void DecodeRle90ArcFull()
+        {
+            /*
+             * The test data for this test was generated using a modified
+             * version of SEA's ARC 5.21p, the modification being that the
+             * output was forced to be compression method 3 (just RLE90). The
+             * file InputData.bin was fed through ARC and the resulting file
+             * was stripped of the ARC header and archive end marker.
+             */
+
+            FileStream rle90file = File.Open("TestData/Rle90ArcEncoded.bin", FileMode.Open);
+            Rle90DecodingStream decoder = new(rle90file, false);
+
+            byte[] buffer = new byte[InputData.Length];
+            decoder.Read(buffer, 0, InputData.Length);
+
+            CollectionAssert.AreEqual(InputData, buffer);
+        }
+    }
+}
