5af9b57da7ee272505d360fc962c46038fcd68dc
Kai NeSmith <nesmithkai@gmail.com>
2024-03-31 22:37:30 -0500

    Add BinHex to Rle90DecodingStreamTests
    Also, "using" statements were used to replace manual disposal.

--- IronFE.Tests/EncodingTests/CompressionTests/Rle90DecodingStreamTests.cs
+++ IronFE.Tests/EncodingTests/CompressionTests/Rle90DecodingStreamTests.cs
@@ -27,11 +27,11 @@ namespace IronFE.Tests.EncodingTests.CompressionTests
              */
 
             FileStream rle90file = File.Open("TestData/Rle90ArcEncoded.bin", FileMode.Open);
-            Rle90DecodingStream decoder = new(rle90file, false);
-
             byte[] buffer = new byte[InputData.Length];
-            decoder.Read(buffer, 0, InputData.Length);
-            decoder.Dispose();
+            using (Rle90DecodingStream decoder = new(rle90file, false))
+            {
+                decoder.Read(buffer, 0, InputData.Length);
+            }
 
             CollectionAssert.AreEqual(InputData, buffer);
         }
@@ -45,21 +45,62 @@ namespace IronFE.Tests.EncodingTests.CompressionTests
         [TestMethod]
         public void DecodeRle90ArcPiecewise()
         {
+            FileStream rle90file = File.Open("TestData/Rle90ArcEncoded.bin", FileMode.Open);
+            byte[] buffer = new byte[InputData.Length];
+            using (Rle90DecodingStream decoder = new(rle90file, false))
+            {
+                for (int i = 0; i < InputData.Length; i++)
+                {
+                    buffer[i] = (byte)decoder.ReadByte();
+                }
+            }
+
+            CollectionAssert.AreEqual(InputData, buffer);
+        }
+
+        /// <summary>
+        /// Tests the functionality of the <see cref="Rle90DecodingStream.ReadInternal(byte[], int, int)"/> method given BinHex-compressed data read in full.
+        /// </summary>
+        [TestMethod]
+        public void DecodeRle90BinHexFull()
+        {
             /*
-             * The test data for this test was generated using a modified
-             * version of SEA's ARC 5.21p, the modification being that the
-             * output was forced to be compression method 3 (just RLE90). The
-             * file InputData.bin was fed through ARC and the resulting file
-             * was stripped of the ARC header and archive end marker.
+             * TODO: Find a BinHex program that natively behaves as expected;
+             * it seems that most implementations use the same approach as ARC,
+             * in that they don't buffer the RLE marker and run it if a run is
+             * indeed found. macutils, binhex.py (and binascii, by extension),
+             * and others have all displayed this behaviour.
+             * For now, we'll once again abuse ARC 5.21p to do our bidding
+             * (output method 3 and buffer RLE marker when compressing).
              */
 
-            FileStream rle90file = File.Open("TestData/Rle90ArcEncoded.bin", FileMode.Open);
-            Rle90DecodingStream decoder = new(rle90file, false);
-
+            FileStream rle90file = File.Open("TestData/Rle90BinHexEncoded.bin", FileMode.Open);
             byte[] buffer = new byte[InputData.Length];
-            for (int i = 0; i < InputData.Length; i++)
+            using (Rle90DecodingStream decoder = new(rle90file, true))
             {
-                buffer[i] = (byte)decoder.ReadByte();
+                decoder.Read(buffer, 0, InputData.Length);
+            }
+
+            CollectionAssert.AreEqual(InputData, buffer);
+        }
+
+        /// <summary>
+        /// Tests the functionality of the <see cref="Rle90DecodingStream.ReadInternal(byte[], int, int)"/> method given BinHex-compressed data read in a piecewise manner.
+        /// </summary>
+        /// <remarks>
+        /// The goal of this test is to evaluate the buffering system the ReadInternal() function implements in order to ensure no errors occur with read boundaries.
+        /// </remarks>
+        [TestMethod]
+        public void DecodeRle90BinHexPiecewise()
+        {
+            FileStream rle90file = File.Open("TestData/Rle90BinHexEncoded.bin", FileMode.Open);
+            byte[] buffer = new byte[InputData.Length];
+            using (Rle90DecodingStream decoder = new(rle90file, true))
+            {
+                for (int i = 0; i < InputData.Length; i++)
+                {
+                    buffer[i] = (byte)decoder.ReadByte();
+                }
             }
 
             CollectionAssert.AreEqual(InputData, buffer);
