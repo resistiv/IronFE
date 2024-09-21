a47ce93c255f51a4615a4e684662b0391cebf6c4
Kai NeSmith <nesmithkai@gmail.com>
2024-09-21 13:21:34 -0500

    Simplify FileBase properties and add manual ctor

--- IronFE/Files/FileBase.cs
+++ IronFE/Files/FileBase.cs
@@ -7,48 +7,50 @@ namespace IronFE.Files
     /// </summary>
     public abstract class FileBase
     {
-        // Private members
-        private readonly Stream fileStream;
-        private readonly string fileFullPath;
-        private readonly string fileName;
-        private FileEntryBase? rootEntry = null;
-
         /// <summary>
         /// Initializes a new instance of the <see cref="FileBase"/> class.
         /// </summary>
         /// <param name="filePath">A path to the file to open and associate with this <see cref="FileBase"/> object.</param>
         protected FileBase(string filePath)
         {
-            fileStream = File.Open(filePath, FileMode.OpenOrCreate);
-
+            Stream = File.Open(filePath, FileMode.OpenOrCreate);
             FileInfo fileInfo = new(filePath);
-            fileFullPath = fileInfo.FullName;
-            fileName = fileInfo.Name;
+            FullPath = fileInfo.FullName;
+            Name = fileInfo.Name;
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="FileBase"/> class.
+        /// </summary>
+        /// <param name="inputStream">A <see cref="System.IO.Stream"/> containing the data of the file.</param>
+        /// <param name="filePath">The absolute path of the file.</param>
+        /// <param name="fileName">The name of the file.</param>
+        protected FileBase(Stream inputStream, string filePath, string fileName)
+        {
+            Stream = inputStream;
+            FullPath = filePath;
+            Name = fileName;
         }
 
         /// <summary>
         /// Gets the <see cref="System.IO.Stream"/> associated with this <see cref="FileBase"/> object.
         /// </summary>
-        public Stream Stream => fileStream;
+        public Stream Stream { get; }
 
         /// <summary>
         /// Gets the full path of the underlying file of this <see cref="FileBase"/> object.
         /// </summary>
-        public string FullPath => fileFullPath;
+        public string FullPath { get; }
 
         /// <summary>
         /// Gets the file name of the underlying file of this <see cref="FileBase"/> object.
         /// </summary>
-        public string Name => fileName;
+        public string Name { get; }
 
         /// <summary>
         /// Gets or sets the root <see cref="FileEntryBase"/> contained within this <see cref="FileBase"/> object.
         /// </summary>
-        public FileEntryBase? Root
-        {
-            get { return rootEntry; }
-            set { rootEntry = value; }
-        }
+        public FileEntryBase? Root { get; protected set; }
 
         /// <summary>
         /// Gets the format name of this <see cref="FileBase"/> object.
